4fbb7d8a6d38c86d5e6a78dc085b5df45827b9c6
«Igor <talovez@yandex.ru>
2022-05-09 15:01:53 +0300

    задан массив и его печать

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array) // метод печать массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($" {array[i]} ,");
+    }
+    Console.WriteLine();
+}
+
+string[] array = { "hello", "-2", "5678", "-))", "fgh" }; // задан массив строк
+Console.WriteLine("Входной массив:");
+PrintArray(array); // печать входного массива
+
