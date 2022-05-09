0b1ee651232b8b96275855275247248b9d839cae
«Igor <talovez@yandex.ru>
2022-05-09 15:38:04 +0300

    нахождение эелементов и печать нового массива

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿void PrintArray(string[] array) // метод печать массива
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write($" {array[i]} ,");
     }
@@ -10,4 +10,18 @@
 string[] array = { "hello", "-2", "5678", "-))", "fgh" }; // задан массив строк
 Console.WriteLine("Входной массив:");
 PrintArray(array); // печать входного массива
-
+int j=0;
+for (int i=0; i < array.Length; i++) // находим количество строк массива с тремя или меньше символами
+{    if (array[i].Length <= 3)
+        j++;
+}
+string[] newarray = new string[j];
+j = 0;
+for (int i=0; i < array.Length; i++) // заполняем новый массив
+{    if (array[i].Length <= 3)
+    {    newarray[j] = array[i];
+        j++;
+    }
+}
+Console.WriteLine("Выходной массив:");
+PrintArray(newarray); // печать нового массива
